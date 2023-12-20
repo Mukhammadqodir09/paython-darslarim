868a8a117feee720dc6251b75430977f585f1b07
MUKHAMMADQODIR <154325811+Mukhammadqodir09@users.noreply.github.com>
2023-12-20 21:55:42 +0500

    phytondan_uyga vazifalarim

---
+++ UY ISHI 1.py
@@ -0,0 +1,15 @@
+"""
+def yoshni_hisoblovchi_dastur(ism,t_yil):
+    print(f" Salom {ism.title()},siz {2023-t_yil} yoshdasiz ")
+yoshni_hisoblovchi_dastur(input("isimni kiriting:"),int(input("Tug`ulgsn yilingizni kiriting:")))
+
+def kvadrat_kib(son):
+    print(f"{son}ni kvadrati {son**2}ga teng \n ",f" {son}ning kubi {son**3}ga teng") 
+kvadrat_kib(int(input("istalgan sonni kiriting:")))
+"""
+def juft_toq(son):
+    if son % 2== 0: 
+         print("bu son juft")
+    else:
+     print("bu son toq")
+juft_toq(int(input("sonni kiriting ")))    
---
+++ uy_ishi_2.py
@@ -0,0 +1,31 @@
+"""
+mashina={"mashina haqida malumot--->":input( "quydagi mashinalardan birini kiriting--->BMW, NEXIA2, DAMAS,"),
+    "Rusumi":"BMW m5",
+    'matori':"s63 V8",
+    'tezlanishi':"0:100gacha 1,8sekund",
+    'yili':"(1986)dan (2023)gacha",
+    
+    "Rusumi":"NEXIA2",
+    'matori':"GM motors",
+    'tezlanishi':"uzoq vaqtda 100ga chiqadi",
+    'yili':"(1990)dan (2023)gacha",
+
+"Rusumi":"DAMAS",
+    'matori':"GM motors",
+    'tezlanishi':"JUDA JUDA UZOQ VAQTDA 100GA CHIQADI",
+    'yili':"LYUBOYIDAN BOR"}    
+print(mashina)
+"""
+x=input("quydagi mashinalardan birini kiriting--->BMW, NEXIA2, DAMAS,")
+y={"Rusumi":"BMW m5",
+'matori':"s63 V8",
+'tezlanishi':"0:100gacha 1,8sekund",
+'yili':"(1986)dan (2023)gacha"}
+z={"Rusumi":"NEXIA2",
+    'matori':"GM motors",
+    'tezlanishi':"uzoq vaqtda 100ga chiqadi",
+    'yili':"(1990)dan (2023)gacha",}
+BMW=y
+NEXIA2=z
+print(x)
+
